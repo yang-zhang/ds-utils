4ed6025c691064346ec0cabc100928d6ecc8ce57
YangZhang <zhangyangcolostate@gmail.com>
2017-03-26 15:06:35 -0400

    Added logloss_scalar

--- ds_utils/evaluation.py
+++ ds_utils/evaluation.py
@@ -2,6 +2,7 @@
 import numpy as np
 import scipy as sp
 import sklearn.metrics
+import math
 
 
 # https://www.kaggle.com/wiki/LogarithmicLoss
@@ -12,6 +13,12 @@ def logloss(act, pred):
     ll = sum(act * sp.log(pred) + sp.subtract(1, act) * sp.log(sp.subtract(1, pred)))
     ll = ll * -1.0 / len(act)
     return ll
+
+
+def logloss_scalar(p, y):
+    epsilon = 1e-15
+    p = min(max(p, epsilon), 1-epsilon)
+    return -math.log(p) if y == 1. else -math.log(1. - p)
 
 
 def mean_absolute_exp_diff(y1, y2):
