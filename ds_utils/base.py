996e9469ee57b568d6d1c226cced15818092e034
YangZhang <zhangyangcolostate@gmail.com>
2017-04-02 14:12:12 -0400

    Add unittest to base

--- ds_utils/base.py
+++ ds_utils/base.py
@@ -1,28 +1,8 @@
 # -*- coding: utf-8 -*-
+import unittest
+
 import numpy as np
 import pandas as pd
-
-
-def make_test_df(n_row=1000):
-    df = pd.DataFrame(
-        {
-            'user_id': list(range(n_row)),
-            'has_churned': np.random.choice([True, False], n_row),
-            'price_plan': np.random.choice(['A', 'B'], n_row),
-            'total_purchase': 20 * np.random.rand(n_row) + 100,
-            'income': 5 * np.random.rand(n_row) + 500,
-            'tax': 2 * np.random.rand(n_row) + 50,
-            'product_purchased': np.random.choice(['X', 'Y', 'Z'], n_row),
-            'region': np.random.choice(['a', 'b', 'c', 'd'], n_row),
-            'date_signed_on': np.random.choice(pd.date_range('2010-01-01', '2016-12-31'), n_row, replace=True).astype(
-                'str'),
-
-        }
-    )
-    df.loc[np.random.choice(n_row, int(n_row * 0.1), replace=False), 'income'] = None
-    df.loc[np.random.choice(n_row, int(n_row * 0.1), replace=False), 'price_plan'] = None
-    df.loc[np.random.choice(n_row, int(n_row * 0.15), replace=False), 'region'] = None
-    return df
 
 
 # R-like table function
@@ -33,11 +13,16 @@ def df_table_r(df, col_1, col_2):
     return tb['_']
 
 
-if __name__ == '__main__':
-    df = make_test_df()
-    print(df.sample(5))
-    print('-' * 50)
+class TestBaseMethods(unittest.TestCase):
+    def test_df_table_r(self):
+        n = 100
+        df = pd.DataFrame({
+            'col_1': np.random.choice([True, False], n),
+            'col_2': np.random.choice(['A', 'B', 'C'], n),
+        })
+        print(df.sample(3))
+        print(df_table_r(df, 'col_1', 'col_2'))
 
-    print('df_table_r')
-    print(df_table_r(df, 'product_purchased', 'has_churned'))
-    print('-' * 50)
+
+if __name__ == '__main__':
+    unittest.main()
