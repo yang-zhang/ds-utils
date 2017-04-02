599122ad6415ef70ed2fae973c82ba54007b6678
YangZhang <zhangyangcolostate@gmail.com>
2017-04-02 07:49:33 -0400

    Added imports

---
+++ ds_utils/imports.py
@@ -0,0 +1,28 @@
+"""Frequent imports and setups. Add the following to the beginning of code:
+    import ds_utils.imports; import imp; imp.reload(ds_utils.imports)
+    from ds_utils.imports import
+
+"""
+
+import sys
+import os
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import sklearn.datasets
+import sklearn.model_selection
+import sklearn.pipeline
+import sklearn.metrics
+
+import sklearn.dummy
+import sklearn.linear_model
+import sklearn.ensemble
+
+import xgboost as xgb
+
+os.environ["KERAS_BACKEND"] = "theano"
+import keras
+from keras import backend as K
+K.set_image_dim_ordering('th')
